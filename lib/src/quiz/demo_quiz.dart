4ecccd07690e25b5b19881e0e114fe76a48c1265
paco <paco.33353@gmail.com>
2021-07-12 20:07:53 -0500

    Intentare hacer un ejemplo en demo

---
+++ lib/src/quiz/demo_quiz.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class DemoQuiz extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hola Mundo'),
+     ),
+   );
+  }
+}
