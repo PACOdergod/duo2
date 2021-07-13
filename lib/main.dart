5284246617f79e7b2e839fba16f563da36eb0693
paco <paco.33353@gmail.com>
2021-07-12 20:16:43 -0500

    creo que ya se como funcionan las coordenadas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:duo2/src/pages/example.dart';
+import 'package:duo2/src/quiz/demo_quiz.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -25,16 +26,16 @@ class MyApp extends StatelessWidget {
         // fontFamily: 
       ),
       title: 'Material App',
-      // home: ExamplePage(),
-      routes: {
-        "auth": (_)=> AuthPage(),
-        "login": (_)=> LoginPage(),
-        "home": (_)=> HomePage(),
-        "lesson": (_)=> LessonPage(),
-        "registrar":(_)=> RegistrarPage(),
-        "final": (_)=> FinalPage(),
-      },
-      initialRoute: "lesson",
+      home: DemoQuiz(),
+      // routes: {
+      //   "auth": (_)=> AuthPage(),
+      //   "login": (_)=> LoginPage(),
+      //   "home": (_)=> HomePage(),
+      //   "lesson": (_)=> LessonPage(),
+      //   "registrar":(_)=> RegistrarPage(),
+      //   "final": (_)=> FinalPage(),
+      // },
+      // initialRoute: "lesson",
     );
   }
 }
--- lib/src/quiz/demo_quiz.dart
+++ lib/src/quiz/demo_quiz.dart
@@ -1,14 +1,55 @@
 import 'package:flutter/material.dart';
 
 
-class DemoQuiz extends StatelessWidget {
+class DemoQuiz extends StatefulWidget {
+
+  @override
+  _DemoQuizState createState() => _DemoQuizState();
+}
+
+class _DemoQuizState extends State<DemoQuiz> {
+
+  bool probando = true;
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: 
+      // Container(
+      //   margin: EdgeInsets.only(
+      //     left: 126,
+      //     top: 282.5
+      //   ),
+      //   height: 39,
+      //   width: 108,
+      //   color: Colors.blue,
+      // ) 
+      Center(
+        child: Boton(),
+      ),
+    );
+  }
+}
+
+class Boton extends StatelessWidget {
+  const Boton({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return GestureDetector(
+      child: Container(
         child: Text('Hola Mundo'),
-     ),
-   );
+        padding: EdgeInsets.all(10),
+        color: Colors.amber,
+      ),
+      onTap: (){
+        final box = context.findRenderObject() as RenderBox;
+        final offset = box.localToGlobal(Offset.zero);
+        print(offset);
+        print(box.size);
+      },
+    );
   }
 }
